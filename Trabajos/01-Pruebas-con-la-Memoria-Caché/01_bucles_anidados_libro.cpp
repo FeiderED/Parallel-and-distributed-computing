1f7b059963b3a3985ee2316c0c06d497e5052cc3
Feider <evaldiviaq@unsa.edu.pe>
2022-09-21 06:04:00 -0500

    Bucles_anidados

--- Trabajos/01-Pruebas-con-la-Memoria-Caché/01_bucles_anidados_libro.cpp
+++ Trabajos/01-Pruebas-con-la-Memoria-Caché/01_bucles_anidados_libro.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
-#include <time.h>
 #include <chrono>
+#include <time.h>
+
+
+using namespace std;
 
 int main (){
-    int MAX = 1000;
-    //std::cout << "Size of the array: ";
-    //std::cin >> MAX;
+    FILE *fp;
+    int size [10] = {50, 100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600};
 
-    
-    double A[MAX][MAX], x[MAX], y[MAX];
-    int i, j;
+    for (int z = 0; z < 10; z++){
+        int actual_size = size[z];
 
-    
-    clock_t start, end;
-    double cpu_time_used;
-    
-    
-    /* Calculate the time of fist loop */
-    //start = clock();
-    for (i = 0; i < MAX; i++){
-        for (j = 0; j < MAX; j++){
-            A[i][j] = i * j;
+        int  **matrix = new int*[actual_size];
+        double *x = new double[actual_size];
+        double *y = new double[actual_size];
+
+        for (int i = 0; i < actual_size; i++){
+            matrix[i] = new int[actual_size];
         }
-    }
-    end = clock(); 
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    std::cout << "Time of first loop: " << cpu_time_used << std::endl;
 
-
-    /* Calculate the time of second loop */
-    start = clock();
-    for (j = 0; j < MAX; j++){
-        for (i = 0; i < MAX; i++){
-            A[i][j] = i * j;
+        
+        auto start = chrono::high_resolution_clock::now();        
+        for (int i = 0; i < actual_size; i++){
+            for (int j = 0; j < actual_size; j++){
+                y[i] += matrix[i][j] * x[j];
+            }
         }
+        auto finish = chrono::high_resolution_clock::now();
+        chrono::duration<double> elapsed = finish - start;
+        cout << "Bucle 1" << " Tamano: " << actual_size << " Tiempo: " << elapsed.count() << " \t";
+
+        auto start2 = chrono::high_resolution_clock::now();
+        for (int j = 0; j < actual_size; j++){
+            for (int i = 0; i < actual_size; i++){
+                y[i] += matrix[i][j] * x[j];
+            } 
+        }
+        auto finish2 = chrono::high_resolution_clock::now();
+        chrono::duration<double> elapsed2 = finish2 - start2;
+        cout << "Bucle 2" << " Tamano: " << actual_size << " Tiempo: " << elapsed2.count() << endl;
+
+
+        fp = fopen("extra_files/bucles_anidados.txt", "a");
+        fprintf(fp, "%d\t%f\t%f\n", actual_size, elapsed.count(), elapsed2.count());
+                
     }
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    std::cout << "Time of second loop: " << cpu_time_used << std::endl;
 
     return 0;
 }
